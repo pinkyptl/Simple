4ccc7c77d87462923e6c41ca4c88bac6d78658cc
pinky <pinkypatel106@gmail.com>
2020-09-29 19:47:36 -0400

    Adding the project

---
+++ src/main/java/com/miit/firstapp/simple/Simple.java
@@ -0,0 +1,14 @@
+package com.miit.firstapp.simple;
+
+public class Simple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=3;
+int b=5;
+int c=a+b;
+
+System.out.println(c);
+	}
+
+}
---
+++ src/test/java/com/miit/firstapp/simple/SimpleTest.java
@@ -0,0 +1,14 @@
+package com.miit.firstapp.simple;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
